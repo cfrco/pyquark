6f86f800b8474bbe1f7c677f843a5663d8a26287
cfrco <cat@cat-Desktop.(none)>
2012-09-13 20:20:51 +0800

    fix : dev/regex

--- pyquark/dev/regex.py
+++ pyquark/dev/regex.py
@@ -2,8 +2,8 @@
 
 import re
 UserID_regex = re.compile("^[a-zA-z][-._a-zA-Z0-9]*$")
-NetMail_regex = re.compile("^[a-zA-z][-._a-zA-Z0-9]*@((?:[a-zA-Z]|[a-zA-Z][-a-zA-Z0-9]*[a-zA-Z0-9]\\.)+[a-zA-Z][a-zA-Z]+$)")
-NetHostname_regex = re.compile("^((?:[a-zA-Z]|[a-zA-Z][-a-zA-Z0-9]*[a-zA-Z0-9]\\.)+[a-zA-Z][a-zA-Z]+$)")
+NetMail_regex = re.compile("^[a-zA-z][-._a-zA-Z0-9]*@((?:(?:[a-zA-Z]|[a-zA-Z][-a-zA-Z0-9]*[a-zA-Z0-9])\\.)+[a-zA-Z][a-zA-Z]+$)")
+NetHostname_regex = re.compile("^((?:(?:[a-zA-Z]|[a-zA-Z][-a-zA-Z0-9]*[a-zA-Z0-9]\\.))+[a-zA-Z][a-zA-Z]+$)")
 
 HttpUrl_regex = re.compile("(https?://(?:[a-z]|[a-z0-9][-a-z0-9]*[a-z0-9]\\.)+[a-z][a-z]+(?::\\d+)?"
 "(?:/[^.!,?;\"'<>()\\[\\]{}\\s\\x7F-\\xFF]*(?:[.!,?]+[^.!,?;\"'<>()\[\]{}\\s\\x7F-\\xFF]+)*)?)",re.I)
